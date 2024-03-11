c3f7d255ed745e734325a2ff16f948c246090a30
ErmiyasHailemichael <Hailermiyas@gmail.com>
2024-03-11 11:01:36 -0700

    mock data

--- Trivia/Triviaquiz.swift
+++ Trivia/Triviaquiz.swift
@@ -15,14 +15,22 @@ struct quizQuestion{
 }
 
 class Triviaquiz: UIViewController {
-
+    
+    var quizQuestions : [quizQuestion] = []
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        mockUpData()
     }
     
-
+    func mockUpData(){
+        let question1 = quizQuestion(question: "What is the capital of Tigray?", choices:["London", "Paris", "Mekelle","Madrid"],correctAnswerIndex: 2)
+        let question2 = quizQuestion(question: "who won the 2023 Ballon d'O?", choices:["Leo Messi", "C.Ronaldo", "Haaland","Vinicius Jr"],correctAnswerIndex: 0)
+        let question3 = quizQuestion(question: "According to Guinness World Records, what's the best-selling book of all time?", choices:["Little Red Book", "Harry Potter and the Philosopher's Stone", "The Lord of the Rings","The bible"],correctAnswerIndex: 3)
+        
+        quizQuestions = [question1, question2, question3]
+    }
     /*
     // MARK: - Navigation
 
