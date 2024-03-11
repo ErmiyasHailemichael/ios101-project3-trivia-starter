5329fcb09e8484550711c00433d4911e463988a0
ErmiyasHailemichael <Hailermiyas@gmail.com>
2024-03-11 10:33:44 -0700

    add view controller, adding labels, linking ui elements to code

---
+++ Trivia/TriviaViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TriviaViewController.swift
+//  Trivia
+//
+//  Created by Ermiyas H on 3/11/24.
+//
+
+import UIKit
+
+class TriviaViewController: UIViewController {
+
+    @IBOutlet weak var choiceLable4: UILabel!
+    @IBOutlet weak var choiceLable3: UILabel!
+    @IBOutlet weak var choiceLable2: UILabel!
+    @IBOutlet weak var choiceLable1: UILabel!
+    @IBOutlet weak var triviaImageView: UIImageView!
+    @IBOutlet weak var categoryLable: UILabel!
+    @IBOutlet weak var questionNumberLable: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Trivia/Triviaquiz.swift
@@ -0,0 +1,36 @@
+//
+//  Triviaquiz.swift
+//  Trivia
+//
+//  Created by Ermiyas H on 3/11/24.
+//
+
+import UIKit
+
+// add mock data structure
+struct quizQuestion{
+    let question: String
+    let choices: [String]
+    let correctAnswerIndex: Int
+}
+
+class Triviaquiz: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
